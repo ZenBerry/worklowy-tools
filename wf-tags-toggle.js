4ae4246c1855d6afb30cbc1ef377902122c93913
Eugene ZenBerry <eugeneberezhnoy@gmail.com>
2023-04-09 08:37:54 +0300

    A bugfix

--- wf-tags-toggle.js
+++ wf-tags-toggle.js
@@ -1,13 +1,30 @@
-function tags() {
-  var elements = document.querySelectorAll('.contentTag');
+function tagsToggleLocal(){ //toggles the tags, works with an open page only
+console.clear();
+let message = ''
+var elements = document.querySelectorAll('.contentTag');
   var displayValue = 'none';
   if (elements.length > 0 && elements[0].style.display === 'none') {
     displayValue = 'initial';
-    console.log('Tags are now switched on');
+    message = 'Tags are now switched on';
   } else {
-    console.log('Tags are now switched off');
-  }
+    message = 'Tags are now switched off';
+}
   for (var i = 0; i < elements.length; i++) {
     elements[i].style.display = displayValue;
   }
+  return message;
 }
+
+function tagsGlobal(control) { //turns the tags on or off based on a parameter, works globally
+  console.clear();
+  if (control === 'on'){
+    document.querySelector('body').insertAdjacentHTML('beforeend', '<style>.contentTag { display: initial; }</style>');
+  } else if (control === 'off') {
+    document.querySelector('body').insertAdjacentHTML('beforeend', '<style>.contentTag { display: none; }</style>');
+  } else {
+    message = 'You have to type: tagsGlobal('on') or tagsGlobal('off')';
+  }
+  return message;
+}
+
+//todo: a global toggle
