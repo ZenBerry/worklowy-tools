d3fe28f9d95e113f626ec66e30a1ed3747a7a37e
Eugene ZenBerry <eugeneberezhnoy@gmail.com>
2023-04-09 06:53:28 +0300

    Wrapped it in a function

--- wf-tags-toggle.js
+++ wf-tags-toggle.js
@@ -1,8 +1,13 @@
-var elements = document.querySelectorAll('.contentTag');
+function tags() {
+  var elements = document.querySelectorAll('.contentTag');
   var displayValue = 'none';
   if (elements.length > 0 && elements[0].style.display === 'none') {
     displayValue = 'initial';
+    console.log('Tags are now switched on');
+  } else {
+    console.log('Tags are now switched off');
   }
   for (var i = 0; i < elements.length; i++) {
     elements[i].style.display = displayValue;
   }
+}
